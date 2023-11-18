b87477cf5d4b721e5952062fccb989a35071aff6
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2023-11-18 13:14:35 +0900

    feat(component): add Layout Class to adapt a Header and Footer throughout the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import Layout from '../components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
+// Layoutは全てのページに適応するために_app.tsxにてComponentを包み込む
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
