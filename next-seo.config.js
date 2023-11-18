b97aede8bc49ad8c029aebcbcbebc6bede6a30f1
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2023-11-18 13:26:30 +0900

    feat(seo): add NextSeo

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Takeaki Blog',
+  description: 'Engineering and Life',
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'TODO',
+    site_name: 'Takeaki Blog'
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import Layout from '../components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import SEO from '../next-seo.config'
+import { DefaultSeo } from 'next-seo'
 
 
 // Layoutは全てのページに適応するために_app.tsxにてComponentを包み込む
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   )
