e6fac64644fb655af25505ed21cd49a51265ebcf
TakeakiMiyazawa <takeakimiyazawa0614@gmail.com>
2023-11-18 13:29:58 +0900

    docs(css): add css comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss'
-
 const config: Config = {
+   /** specify folders and files that apply tailwind css*/
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  /** Depends on theme, Tailwind generates css*/
+  /** Theme parameters: colors, screen font, brakepoint, spacing*/
   theme: {
     extend: {
       backgroundImage: {
